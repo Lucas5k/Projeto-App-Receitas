462fbe1f06f2ed0a34860b0b9113d8860581653d
Leo Araujo <leonardo_02452@hotmail.com>
2022-04-26 16:41:45 -0300

    Req 3, 4 e 5 - criado estados locais para controlar form e habilitar o botão

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [login, setLogin] = useState({ email: '', password: '' });
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const handleLogin = ({ target }) => {
+    const { name, value } = target;
+    setLogin({ ...login, [name]: value });
+  };
+
+  useEffect(() => {
+    const enableButton = () => {
+      const { email, password } = login;
+      const emailFormat = /[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]*\w$/;
+      const isEmailValid = emailFormat.test(email);
+      const minPasswordCaracters = 6;
+      const isPasswordValid = password.length >= minPasswordCaracters;
+      setIsButtonDisabled(!(isEmailValid && isPasswordValid));
+    };
+    enableButton();
+  }, [login]);
+
   return (
     <form>
-      <input type="text" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <button type="submit" data-testid="login-submit-btn">Enter</button>
+      <label htmlFor="email-input">
+        Email
+        <input
+          type="text"
+          data-testid="email-input"
+          id="email-input"
+          name="email"
+          value={ login.email }
+          onChange={ handleLogin }
+        />
+      </label>
+      <label htmlFor="password-input">
+        password
+        <input
+          type="password"
+          data-testid="password-input"
+          id="password-input"
+          name="password"
+          value={ login.password }
+          onChange={ handleLogin }
+        />
+      </label>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ isButtonDisabled }
+      >
+        Enter
+      </button>
     </form>
   );
 }
