e3644f479dab318beef8d7c07b2e5de210f6bd41
Veronica-Alfr <veagalves@gmail.com>
2022-04-26 18:40:47 -0300

    Req 10 - Icones de perfil e busca adds

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import profileIcon from '../images/profileIcon.svg'
+import searchIcon from '../images/searchIcon.svg'
 
 function Foods() {
   <header>
-    <h1 data-testid="page-title">Recipes Cook</h1>
-    <button
-    type="button"
-    data-testid="profile-top-btn"
-    >
-    Profile
-    </button>
-    <button
-    type="button"
-    data-testid="search-top-btn"
-    >
-    Search
-    </button>
-    <img src="" alt="" />
+      <h1 data-testid="page-title">Recipes Cook</h1>
+      <button
+      type="button"
+      data-testid="profile-top-btn"
+      >
+      Profile
+      </button>
+      <img src={ profileIcon } alt="Profile Icon" />
+      <button
+      type="button"
+      data-testid="search-top-btn"
+      >
+      Search
+      </button>
+      <img src={ searchIcon } alt="Search Icon"/>
   </header>
 }
 
