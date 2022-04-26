9e5a677f6b91054910108a45571a6b4ad3c27eec
Veronica-Alfr <veagalves@gmail.com>
2022-04-26 18:47:12 -0300

    Req 9-10 - Icones e Header arrumados

---
+++ src/context/pages/Drinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Drinks() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
+
+export default Drinks;
---
+++ src/context/pages/Foods.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Foods() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
+
+export default Foods;
