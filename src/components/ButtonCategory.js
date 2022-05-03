84aff4d8dd38ba4274e0eb82749fed11db918c0d
Lucas5k <lucascabral112347@gmail.com>
2022-05-02 21:35:55 -0300

    Req 27 ao 30

---
+++ src/components/ButtonCategory.js
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import contextGlobal from '../context';
+
+function ButtonCategory({ category }) {
+  const [checkingDrinks, setCheckingDrinks] = useState([]);
+
+  const { strCategory } = category;
+  const { foodFilter, drinkFilter } = useContext(contextGlobal);
+
+  const { pathname } = useLocation();
+  const handleClick = ({ target }) => {
+    setCheckingDrinks([...checkingDrinks, target.value]);
+    const checking = checkingDrinks.some((el) => el === target.value);
+    return pathname === '/foods'
+      ? foodFilter(target.value, checking) : drinkFilter(target.value, checking);
+  };
+
+  return (
+    <section>
+      <button
+        type="button"
+        onClick={ handleClick }
+        value={ strCategory }
+        data-testid={ `${strCategory}-category-filter` }
+      >
+        { strCategory }
+      </button>
+    </section>
+  );
+}
+
+ButtonCategory.propTypes = {
+  category: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default ButtonCategory;
