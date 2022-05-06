d697a26131c29fad3b4162058db49725dd0d1068
Bruna <brunac.cardososilva@gmail.com>
2022-05-06 18:16:34 -0300

    req 59 concluído.

--- src/components/DoneRecipeCard.js
+++ src/components/DoneRecipeCard.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ShareButton from './ShareButton';
 
 function DoneRecipeCard({ doneRecipe, index }) {
   return (
     <div>
-      <img
-        data-testid={ `${index}-horizontal-image` }
-        src={ doneRecipe.image }
-        alt="recipe-card"
-      />
+      <Link
+        to={ `/${doneRecipe.type}s/${doneRecipe.id}` }
+      >
+        <img
+          width={ 200 }
+          data-testid={ `${index}-horizontal-image` }
+          src={ doneRecipe.image }
+          alt="recipe-card"
+        />
+      </Link>
+      <Link to={ `/${doneRecipe.type}s/${doneRecipe.id}` }>
+        <span data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</span>
+      </Link>
       <span
         data-testid={ `${index}-horizontal-top-text` }
       >
@@ -17,7 +26,6 @@ function DoneRecipeCard({ doneRecipe, index }) {
           ? `${doneRecipe.nationality} - ${doneRecipe.category}`
           : doneRecipe.alcoholicOrNot}
       </span>
-      <span data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</span>
       <span data-testid={ `${index}-horizontal-done-date` }>{doneRecipe.doneDate}</span>
       <ShareButton
         index={ index }
