af5b94d10e50c6f23010c84dace1bd7a6f2cee29
Leo Araujo <leonardo_02452@hotmail.com>
2022-05-09 15:36:07 -0300

    Renderizado os botões de filtro

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NewHeader from '../components/NewHeader';
+import FiltersButtons from '../components/FiltersButtons';
 
 function FavoritesRecipes() {
   return (
     <main>
       <NewHeader name="Favorite Recipes" />
+      <FiltersButtons />
     </main>
   );
 }
