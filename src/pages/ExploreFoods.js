a2f57d407151e9c0f2275dcbfd7caf6cbf70d900
matheusjesse <matheus_jesse@hotmail.com>
2022-05-12 01:22:43 -0300

    componentizando a page ExploreFoods e aplicando css

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -3,43 +3,46 @@ import { Link, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import NewHeader from '../components/NewHeader';
 import contextGlobal from '../context';
+import ContainerExploreFoods from '../styles/ExploreFoods';
 
 function ExploreFoods() {
   const { handleRandom, resultsRandomFoods } = useContext(contextGlobal);
   return (
     <div>
       <NewHeader name="Explore Foods" />
-      <Link
-        to="/explore/foods/ingredients"
-        data-testid="explore-by-ingredient"
-      >
-        By Ingredient
-      </Link>
-      <Link
-        to="/explore/foods/nationalities"
-        data-testid="explore-by-nationality"
-      >
-        By Nationality
-      </Link>
-      <button
-        type="button"
-        data-testid="explore-surprise"
-        onClick={ handleRandom }
-      >
-        Surprise me!
-      </button>
+      <ContainerExploreFoods>
+        <Link
+          to="/explore/foods/ingredients"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </Link>
+        <Link
+          to="/explore/foods/nationalities"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleRandom }
+        >
+          Surprise me!
+        </button>
 
-      {
-        resultsRandomFoods && resultsRandomFoods.map((food, index) => {
-          const { idMeal } = food;
-          return (
-            <Redirect
-              key={ index }
-              to={ `/foods/${idMeal}` }
-            />
-          );
-        })
-      }
+        {
+          resultsRandomFoods && resultsRandomFoods.map((food, index) => {
+            const { idMeal } = food;
+            return (
+              <Redirect
+                key={ index }
+                to={ `/foods/${idMeal}` }
+              />
+            );
+          })
+        }
+      </ContainerExploreFoods>
       <Footer />
     </div>
   );
