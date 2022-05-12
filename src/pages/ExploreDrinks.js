480c8fc6f0f43d265992fb96186632a4744f215e
matheusjesse <matheus_jesse@hotmail.com>
2022-05-12 01:25:39 -0300

    componentizando pagina de ExploreDrinks e aplicando css

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -3,37 +3,40 @@ import { Link, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
 import NewHeader from '../components/NewHeader';
 import contextGlobal from '../context';
+import ContainerExploreDrinks from '../styles/ExploreDrinks';
 
 function ExploreDrinks() {
   const { handleRandom, resultsRandomDrinks } = useContext(contextGlobal);
   return (
     <main>
       <NewHeader name="Explore Drinks" />
+      <ContainerExploreDrinks>
+        <Link
+          to="/explore/drinks/ingredients"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleRandom }
+        >
+          Surprise me!
+        </button>
+        {
+          resultsRandomDrinks && resultsRandomDrinks.map((drink, index) => {
+            const { idDrink } = drink;
+            return (
+              <Redirect
+                key={ index }
+                to={ `/drinks/${idDrink}` }
+              />
+            );
+          })
+        }
+      </ContainerExploreDrinks>
       <Footer />
-      <Link
-        to="/explore/drinks/ingredients"
-        data-testid="explore-by-ingredient"
-      >
-        By Ingredient
-      </Link>
-      <button
-        type="button"
-        data-testid="explore-surprise"
-        onClick={ handleRandom }
-      >
-        Surprise me!
-      </button>
-      {
-        resultsRandomDrinks && resultsRandomDrinks.map((drink, index) => {
-          const { idDrink } = drink;
-          return (
-            <Redirect
-              key={ index }
-              to={ `/drinks/${idDrink}` }
-            />
-          );
-        })
-      }
     </main>
   );
 }
---
+++ src/styles/ExploreDrinks.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const ContainerExploreDrinks = styled.div`  
+  background-color: var(--white);
+  margin-top: 90px;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;    
+    margin: 22px 0px 0px 0px;
+    height: 60px;
+    width: 200px;
+    text-decoration: none;
+    background-color: var(--white);
+    color: var(--orange);
+    border-radius: 8px;
+    box-shadow: 1px 1px 3px 1px var(--orange);
+    font-size: 20px;
+    font-family: 'Merienda', cursive;
+    margin-left: 92px;
+  }
+
+  button {    
+    margin: 22px 0px 0px 0px;
+    height: 60px;
+    width: 200px;
+    border: none;
+    color: var(--black);
+    background-color: var(--white);
+    color: var(--orange);
+    border-radius: 8px;
+    box-shadow: 1px 1px 3px 1px var(--orange);
+    font-family: 'Merienda', cursive;
+    font-size: 20px;
+    margin-left: 92px;
+  }
+
+`;
+
+export default ContainerExploreDrinks;
